8911a8c9840cad89ece337a403d642c8b0490cf4
DarkChina <aaa@g.com>
2019-06-20 00:49:07 +0300

    rendering edited part 1

--- src/components/instruments/priceline.jsx
+++ src/components/instruments/priceline.jsx
@@ -5,42 +5,40 @@ export default class PriceLine extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            symbolName: '',
-            lastPrice: 0,
-            lastVolume: 0
-        }
-        this.hitbtcSocket = new WebSocket('wss://api.hitbtc.com/api/2/ws')
-        this.subscribeTicker = this.subscribeTicker.bind(this)
-      }
-
-      componentDidMount() {
-        this.hitbtcSocket.onopen = () => this.subscribeTicker(this.hitbtcSocket, 'subscribeTicker', {symbol: this.props.symbol})
-        this.hitbtcSocket.onmessage = msg => {
-            const data = JSON.parse(msg.data)
-            if(data.params !== undefined) {
-                this.setState({
-                    symbolName: data.params.symbol,
-                    lastPrice: data.params.last, 
-                    lastVolume: data.params.volume
-                })
-             }
-        }
-    }
-
-    subscribeTicker(ws, method, params = {}) {
-        let id = 1;
-        if (ws.readyState === 1) {
-            const msg = JSON.stringify({method, params, id: id++});
-            ws.send(msg);
+            symbolName: this.props.symbolName,
+            lastPrice: this.props.symbolPrice,
+            lastVolume: this.props.symbolVolume
         }
       }
+
+    //   componentDidMount() {
+    //     this.hitbtcSocket.onopen = () => this.subscribeTicker(this.hitbtcSocket, 'subscribeTicker', {symbol: this.props.symbol})
+    //     this.hitbtcSocket.onmessage = msg => {
+    //         const data = JSON.parse(msg.data)
+    //         if(data.params !== undefined) {
+    //             this.setState({
+    //                 symbolName: data.params.symbol,
+    //                 lastPrice: data.params.last, 
+    //                 lastVolume: data.params.volume
+    //             })
+    //          }
+    //     }
+    // }
+
+    // subscribeTicker(ws, method, params = {}) {
+    //     let id = 1;
+    //     if (ws.readyState === 1) {
+    //         const msg = JSON.stringify({method, params, id: id++});
+    //         ws.send(msg);
+    //     }
+    //   }
 
       render() {
           return (
             <Row>
-                <Col>{this.state.symbolName}</Col>
-                <Col>{this.state.lastPrice}</Col>
-                <Col>{this.state.lastVolume}</Col>
+                <Col>{this.props.symbolName}</Col>
+                <Col>{this.props.lastPrice}</Col>
+                <Col>{this.props.lastVolume}</Col>
             </Row>
           )
       }
--- src/components/instruments/pricetable.jsx
+++ src/components/instruments/pricetable.jsx
@@ -8,15 +8,53 @@ export default class PriceTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            symbols: symbols.map(symbol => symbol.id)
+            renderedList: [],
+            symbolName: '',
+            lastPrice: 0, 
+            lastVolume: 0
         }
+        this.hitbtcSocket = new WebSocket('wss://api.hitbtc.com/api/2/ws')
+        this.subscribeTicker = this.subscribeTicker.bind(this)
     }
+
+    componentDidMount() {
+        const symbolsArr = symbols.map(symbol => symbol.id);
+        this.hitbtcSocket.onopen = () => symbolsArr.forEach(s => this.subscribeTicker(this.hitbtcSocket, 'subscribeTicker', {symbol: s}))
+
+
+
+        this.hitbtcSocket.onmessage = msg => {
+            const data = JSON.parse(msg.data)
+            if(data.params !== undefined) {
+                const renderedList = this.state.renderedList;
+                this.setState({
+                    symbolName: data.params.symbol,
+                    lastPrice: data.params.last, 
+                    lastVolume: data.params.volume,
+                    renderedList: !renderedList.includes(data.params.symbol) ? renderedList.concat(data.params.symbol) : renderedList
+                })
+            }
+        } 
+    }
+
+    subscribeTicker(ws, method, params = {}) {
+        let id = 1;
+        if (ws.readyState === 1) {
+            const msg = JSON.stringify({method, params, id: id++});
+            ws.send(msg);
+        }
+      }
 
     render() {
         return (
-            <div>
-                {this.state.symbols.map((symbol, i) => <PriceLine key={i} symbol={symbol}/>)}               
+            <div> 
+                {
+                    this.state.renderedList.map(element => <PriceLine symbolName={this.state.symbolName} lastPrice={this.state.lastPrice} lastVolume={this.state.lastVolume} />)
+                }
+                <p>{this.state.renderedList}</p>
             </div>
         );
     }
 }
+
+//{this.state.symbols.map((symbol, i) => <PriceLine key={i} symbol={symbol}/>)}
